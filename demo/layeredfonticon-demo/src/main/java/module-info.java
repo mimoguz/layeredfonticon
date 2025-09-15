51b34c60fa4f14eecaf14ccb469e1ded0e628648
mimoguz <mimoguz@users.noreply.github.com>
2025-09-15 18:11:23 +0300

    Remove jspecify dependency from demo projects

--- demo/layeredfonticon-demo/src/main/java/module-info.java
+++ demo/layeredfonticon-demo/src/main/java/module-info.java
@@ -1,10 +1,6 @@
-import org.jspecify.annotations.NullMarked;
-
-@NullMarked
 open module io.github.mimoguz.layeredfonticon.demo {
 
     requires io.github.mimoguz.layeredfonticon;
     requires java.desktop;
-    requires org.jspecify;
 
 }
--- demo/layeredfonticon-flat-demo/src/main/java/module-info.java
+++ demo/layeredfonticon-flat-demo/src/main/java/module-info.java
@@ -1,12 +1,8 @@
-import org.jspecify.annotations.NullMarked;
-
-@NullMarked
 open module flatdemo {
 
     requires io.github.mimoguz.layeredfonticon;
     requires io.github.mimoguz.layeredfonticon.flat;
     requires java.desktop;
-    requires org.jspecify;
     requires com.formdev.flatlaf;
 
 }
